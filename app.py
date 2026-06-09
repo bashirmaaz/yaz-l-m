c08c8b3d482857c26a3712d43b7ad79e7f019e6e
ahmet <bashir121maaz@gmail.com>
2026-06-10 01:36:40 +0300

    Use dynamic port binding from environment variable for Render

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ if __name__ == '__main__':
     create_tables()
 
     # Flask uygulamasını çalıştırma 
-    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
